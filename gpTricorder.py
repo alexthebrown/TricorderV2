a8d3e684637cfbee4c0f5f6130da3773d5904bf3
alexthebrown <99758041+alexthebrown@users.noreply.github.com>
2024-06-23 22:46:28 -0500

    framerate

--- gpTricorder.py
+++ gpTricorder.py
@@ -331,7 +331,7 @@ def update_frame():
 
             canvas.create_image(0,0,anchor=tk.NW,image=frame)
             canvas.image = frame
-            window.after(10, update_frame)
+            window.after(1, update_frame)
         
     # if not is_stopped:
     #     window.after(10, update_frame)
