31a89e1243725d35544c956d388db5299ee5f2ee
alexthebrown <99758041+alexthebrown@users.noreply.github.com>
2024-06-23 22:26:12 -0500

    No more Bayer

--- gpTricorder.py
+++ gpTricorder.py
@@ -324,7 +324,7 @@ def update_frame():
     if not is_paused and not is_stopped:
         ret, frame = cap.read()
         if ret:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BG2BGR)
             frame = Image.fromarray(frame)
             frame = ImageTk.PhotoImage(frame)
 
