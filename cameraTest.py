e05bb44ace7ef33599ecfdc8b7c381fdfeda1693
alexthebrown <99758041+alexthebrown@users.noreply.github.com>
2024-06-27 23:48:16 -0500

    Update cameraTest.py

--- cameraTest.py
+++ cameraTest.py
@@ -1,5 +1,4 @@
-from picamera2 import Picamera2, Preview
-from libcamera import Transform
+from picamera2 import Picamera2
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL, x=100, y=200, width=800, height=600, transform=Transform(hflip=1))
-picam2.start()
+picam2.start(show_preview=True)
+picam2.title_fields = ["ExposureTime", "AnalogueGain"]
